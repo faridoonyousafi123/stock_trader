032b8e67f7f55de4630d6cc91e8948f8785805b9
yousafi <40589224+yousafi@users.noreply.github.com>
2019-03-26 15:12:09 +0430

    first commit

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <app-stock :stock="stock" v-for="stock in stocks" :key="stock.id">
+
+        </app-stock>
+    </div>
+</template>
+
+<script>
+import Stock from './Stock.vue'
+export default {
+    components: {
+        appStock: Stock
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import store from './store/store'
+import { routes } from './routes'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+})
